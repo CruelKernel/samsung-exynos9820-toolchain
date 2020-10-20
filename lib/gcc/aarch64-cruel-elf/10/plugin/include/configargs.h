6e6b818a70395ce51675ecc712dd96afad3b18a3
Denis Efremov <efremov@linux.com>
2020-10-20 13:15:22 +0300

    GCC 10.2, binutils 2.35.1
    GCC 10.2, binutils 2.35.1 system toolchain for aarch64.
    Built with LTO+PGO.
    
    Signed-off-by: Denis Efremov <efremov@linux.com>

---
+++ lib/gcc/aarch64-cruel-elf/10/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
